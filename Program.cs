a01c61c4d7a921767d11d7334d78cff7252a89d6
KumarSaroj99 <sarojkumar.panda@aurionpro.com>
2024-07-12 21:30:02 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxingUnboxing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 10;
+            Console.WriteLine("The number is : "+number);
+
+            object obj = "Hello";
+            Console.WriteLine("Object contains : "+obj);
+            //value type --> reference
+            obj = number;
+            Console.WriteLine("Now the object contains : "+obj+ " ," +
+                "after value type --> reference casting.");
+            //reference --> value type
+            number = (int)obj;
+            Console.WriteLine("Now the number contains : " + obj + " ," +
+                "after reference --> value type casting.");
+        }
+    }
+}
